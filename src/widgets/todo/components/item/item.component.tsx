cdf4a8fa5528d43bccc1887b47308be0114a87fc
Shamil <zagirovshamil@gmail.com>
2025-04-04 00:54:05 +0300

    add scss fitches and add ant checkbox

--- src/widgets/todo/components/item/item.component.tsx
+++ src/widgets/todo/components/item/item.component.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { useAppDispatch } from "../../../../app/store/hook";
 import { removeTodo } from "../../../../app/store/todoSlice";
+import { Checkbox } from "antd";
 import "./item.modules.scss";
 
 interface TodoItemProps {
@@ -9,11 +11,20 @@ interface TodoItemProps {
 
 export const TodoItem: React.FC<TodoItemProps> = ({ id, title }) => {
   const dispatch = useAppDispatch();
+  const [checked, setChecked] = useState(Boolean);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event);
+  };
+
   return (
-    <li key={id}>
-      <input type="checkbox" />
-      <span>{title}</span>
-      <span className="delete" onClick={() => dispatch(removeTodo(id))}>
+    <li className="container__item" key={id}>
+      <Checkbox onChange={(event) => handleChange(event)} />
+      <span className={"container__item-deleteButton"}>{title}</span>
+      <span
+        className="container__item-deleteButton"
+        onClick={() => dispatch(removeTodo(id))}
+      >
         &times;
       </span>
     </li>
