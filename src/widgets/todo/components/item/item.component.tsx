d7a674e91e1e2393da536293ff7da9fd77145733
Shamil <zagirovshamil@gmail.com>
2025-04-04 13:20:26 +0300

    update delete icon and logic delete tasks

--- src/widgets/todo/components/item/item.component.tsx
+++ src/widgets/todo/components/item/item.component.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { useAppDispatch } from "../../../../app/store/hook";
 import { removeTodo } from "../../../../app/store/todoSlice";
-import { Checkbox } from "antd";
+import { Checkbox, message, Popconfirm, PopconfirmProps } from "antd";
 import "./item.modules.scss";
+import { DeleteOutlined } from "@ant-design/icons";
 
 interface TodoItemProps {
   id: string;
@@ -14,19 +15,32 @@ export const TodoItem: React.FC<TodoItemProps> = ({ id, title }) => {
   const [checked, setChecked] = useState(Boolean);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(event);
+    setChecked(event.target.checked);
+  };
+
+  const deleteTask: PopconfirmProps["onConfirm"] = () => {
+    dispatch(removeTodo(id));
+    message.success("Task deleted successfully!");
+  };
+
+  const cancel: PopconfirmProps["onCancel"] = (e) => {
+    message.error("Click on No");
   };
 
   return (
     <li className="container__item" key={id}>
       <Checkbox onChange={(event) => handleChange(event)} />
-      <span className={"container__item-deleteButton"}>{title}</span>
-      <span
-        className="container__item-deleteButton"
-        onClick={() => dispatch(removeTodo(id))}
+      <span className={checked ? "line" : ""}>{title}</span>
+      <Popconfirm
+        title="Удалить задачу?"
+        description="Действительно хотите удалить задачу?"
+        onConfirm={deleteTask}
+        onCancel={cancel}
+        okText="Yes"
+        cancelText="No"
       >
-        &times;
-      </span>
+        <DeleteOutlined />
+      </Popconfirm>
     </li>
   );
 };
